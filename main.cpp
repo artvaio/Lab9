d597aa5092f7e899f951017b9b7ac2aa813833fa
artvaio <bmw-0099@mail.ru>
2023-11-25 23:21:51 +0300

    add input two int

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+    int first, second;
+    std::cout<<"Enter the first num:"<<endl;
+    std::cin>>first;
+    std::cout<<"Enter the second num:"<<endl;
+    std::cin>>second;
     return 0;
 }
